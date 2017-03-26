6bae87f399dfbc0ffcbe604a4a3242824be2a4ab
LaHesis <cryptograph2013@yandex.ru>
2017-03-27 06:51:51 +0700

    myenv was deleted from tracking through rm --cache, also urls.py, views.py, settings.py, /mysite/urls.py was added or changed

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h*u08aq)pofc@0fb!ni*wvlshr^6u0c679^*4^r^^ug+m#vvsg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['happython.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'happython.pythonanywhere.com', '127.0.0.1:8000']
 
 
 # Application definition
